57211ccb5c056febcdce4183cdc0a5bc4e7b965e
Rafael Fernández López <ereslibre@ereslibre.es>
2021-06-10 11:06:04 +0200

    Bump policy-sdk-rust dependency

--- src/settings.rs
+++ src/settings.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 // Describe the settings your policy expects when
 // loaded by the policy server.
 #[derive(Serialize, Deserialize, Default, Debug)]
+#[serde(default)]
 pub(crate) struct Settings {}
 
 impl kubewarden::settings::Validatable for Settings {
